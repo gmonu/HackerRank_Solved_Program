f9e49768c78476c3885edd14ee9e6a81948f8e80
= <=>
2020-02-14 01:00:20 +0530

    Second program

---
+++ src/Java/Pattern1.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i =1; i<=10; i++)
+		{
+			for(int j = 1; j<=10; j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println(" ");
+	
+		}
+	}	
+
+}
